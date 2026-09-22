98a4aed258d2ea81b1feff6d1153edf5d87503b1
Equestrian Labs <arojas@corroshop.com>
2026-09-21 23:37:14 -0400

    Add files via upload

--- src/shopifyClient.js
+++ src/shopifyClient.js
@@ -31,7 +31,9 @@ class ShopifyClient {
       .replace(/^https?:\/\//, "")
       .replace(/\/$/, "")
       .trim();
-    const version = apiVersion || "2025-10";\n    this.baseUrl = `https://${cleanStore}/admin/api/${version}`;\n    console.log(`[${brandKey}] Shopify endpoint: ${this.baseUrl}`);
+    const version = apiVersion || "2025-10";
+    this.baseUrl = `https://${cleanStore}/admin/api/${version}`;
+    console.log(`[${brandKey}] Shopify endpoint: ${this.baseUrl}`);
     this.http = axios.create({
       baseURL: this.baseUrl,
       headers: {
